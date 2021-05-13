697fb9f1276e453c200026cd765f7c2462ea8a4d
Gabriel Bernard Lee <gabriel@Gabriels-MacBook-Pro.local>
2021-05-13 22:39:56 +0800

    Completed Chapters 1 through 8

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import Search from './Search';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './Login';
+
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <Redirect to="/new/1" />}
+        />
+
+        <Route
+          exact
+          path="/create"
+          component={CreateLink}
+        />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
+    </div>
+  </div>
+);
+
+export default App;
